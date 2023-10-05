90383732aa598cc7112b835169f508333d8e89e8
Monerat <gmonerat@gmail.com>
2023-10-05 19:41:55 -0300

    Adicionando informação a documentação do Swagger

--- futebol/src/main/java/br/com/grupo3/futebol/config/SwaggerConfig.java
+++ futebol/src/main/java/br/com/grupo3/futebol/config/SwaggerConfig.java
@@ -2,8 +2,10 @@ package br.com.grupo3.futebol.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
+
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -13,9 +15,18 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+                .apis(RequestHandlerSelectors.basePackage("br.com.grupo3"))
+                .build()
+                .apiInfo(apiInfo());
     }
 
+    private ApiInfo apiInfo(){
+        ApiInfo apiInfo = new ApiInfoBuilder()
+        .title("API de Teste")
+        .description("Essa é uma API desenvolvida para a apresentação, treino e estudo do Grupo 3")
+        .version("1.0.0")
+        .build();
+        
+        return apiInfo;
+    }
 }
