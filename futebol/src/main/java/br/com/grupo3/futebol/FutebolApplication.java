40760e0553e099a7c7300f2c03a6207e1ed824d0
Monerat <gmonerat@gmail.com>
2023-10-04 21:45:55 -0300

    Iniciando a API com Spring boot

---
+++ futebol/src/main/java/br/com/grupo3/futebol/FutebolApplication.java
@@ -0,0 +1,13 @@
+package br.com.grupo3.futebol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutebolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutebolApplication.class, args);
+	}
+
+}
