1f2bbc375030e7467f4c090ecfc18390d8f44bc5
tictoneli <raynan.castro@aluno.senai.br>
2023-10-05 18:19:56 -0300

    imagem png da classe image linkado com classe time

--- futebol/src/main/java/br/com/grupo3/futebol/repository/TimeRepository.java
+++ futebol/src/main/java/br/com/grupo3/futebol/repository/TimeRepository.java
@@ -1,55 +1,73 @@
 package br.com.grupo3.futebol.repository;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
 import br.com.grupo3.futebol.model.Time;
+import br.com.grupo3.futebol.model.exceptions.ResourceNotFound;
 
 @Repository
 public class TimeRepository {
     private List<Time> times = new ArrayList<>();
     private long ultimoID = 0;
 
-    //Crud
+    // Crud
 
-    //Create
-    public Time add(Time time){
+    // Create
+    public Time add(Time time) {
         ultimoID++;
         time.setId(ultimoID);
+        time.setEscudo(verifEscudo(time.getNome() + ".png"));
         times.add(time);
 
         return time;
     }
 
-    //Read
-    //Read all
-    public List<Time> getAll (){
+
+    public String verifEscudo(String fileName) {
+
+        // Get the path to the folder where you want to lookup the file
+        String folderPath = "src/main/java/br/com/grupo3/futebol/base/";
+
+        // Create a File object for the file
+        File file = new File(folderPath + fileName);
+
+        // Check if the file exists
+        if (file.exists()) {
+            return file.getAbsolutePath();
+        } throw new ResourceNotFound("Arquivo não encontrado na base com o nome: "+ fileName);
+    }
+
+    // Read
+    // Read all
+    public List<Time> getAll() {
         return times;
     }
 
-    //Read id
-    public Time get(long id){
+    // Read id
+    public Time get(long id) {
         Time timeFound = null;
         for (Time time : times) {
-            if (time.getId()==id){
+            if (time.getId() == id) {
                 timeFound = time;
             }
         }
         return timeFound;
     }
 
-    //Update
-    public Time update(Time time){
-        times.removeIf(t -> t.getId()==time.getId());
+    // Update
+    public Time update(Time time) {
+        times.removeIf(t -> t.getId() == time.getId());
         times.add(time);
 
         return time;
     }
 
-    //Delete
-    public void delete(long id){
-        times.removeIf(t -> t.getId()==id);
+    // Delete
+    public void delete(long id) {
+        times.removeIf(t -> t.getId() == id);
     }
 }
