afa315f989e268ca9babc61f30ca3e5afb863808
Monerat <gmonerat@gmail.com>
2023-10-05 18:29:18 -0300

    Raynan: Linkou a classe Time com a imagem no banco de dados

--- futebol/src/main/java/br/com/grupo3/futebol/repository/TimeRepository.java
+++ futebol/src/main/java/br/com/grupo3/futebol/repository/TimeRepository.java
@@ -1,11 +1,13 @@
 package br.com.grupo3.futebol.repository;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
 import br.com.grupo3.futebol.model.Time;
+import br.com.grupo3.futebol.model.exceptions.ResourceNotFound;
 
 @Repository
 public class TimeRepository {
@@ -15,12 +17,24 @@ public class TimeRepository {
     //Crud
 
     //Create
-    public Time add(Time time){
+    public Time add(Time time) {
         ultimoID++;
         time.setId(ultimoID);
+        time.setEscudo(verifEscudo(time.getNome() + ".png"));
         times.add(time);
 
         return time;
+    }
+
+
+    public String verifEscudo(String fileName) {
+        String folderPath = "src/main/java/br/com/grupo3/futebol/base/";
+        File file = new File(folderPath + fileName);
+        
+        if (file.exists()) {
+            return file.getAbsolutePath();
+        } throw new ResourceNotFound("Arquivo não encontrado na base com o nome: "+ fileName);
+    
     }
 
     //Read
